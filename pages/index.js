e81d5171857c6cb6211154e902d42543f2b36578
Arturo Herrero <blksmithco@gmail.com>
2022-06-14 20:48:46 -0400

    Test Commit

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Bienvenidos to <a href="https://nextjs.org">Next.js!</a>
+          Bienvenidos To <a href="https://nextjs.org">Next.js!!</a>
         </h1>
 
         <p className={styles.description}>
